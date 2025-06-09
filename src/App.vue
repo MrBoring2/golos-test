c02f2836e4b1b024444c2f093ed94c29215018d0
MrBoring <denis_simonov1337@mail.ru>
2025-06-09 19:18:45 +0500

    add header styles

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,170 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  import { RouterLink, RouterView } from 'vue-router'
+  import HelloWorld from './components/HelloWorld.vue'
+  import EcosystemIcon from './components/icons/IconEcosystem.vue'
+  import IconPhone from './components/icons/IconPhone.vue'
+  import logo from './assets/logo.png'
+  import WelcomeItem from './components/WelcomeItem.vue'
+  
+
+  export default {
+    components: {
+      RouterLink,
+      RouterView,
+      HelloWorld
+    }
+  }
 </script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+<template> 
+  <div class="wrapper">
+    <header>
+      <img class="logo" src="./assets/logo.png" width="140px"></img>
+      <div class="ads">
+        <p>Семейная ипотека 3.5%</p>
+      </div>
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+        <RouterLink>Выбор квартир</RouterLink>
+        <RouterLink>Способы покупки</RouterLink>
+        <RouterLink>Ход строительства</RouterLink>
+        <RouterLink>Ещё</RouterLink>
+      </nav>  
+      
+      <div class="phone-number">
+        <a>+7 (351) 778-78-00</a>
+      </div>
 
-  <RouterView />
+      <div class="icons">     
+        <div class="icon">
+          <font-awesome-icon icon="phone" size="2xl"/>
+        </div>
+      <div class="icon">
+        <font-awesome-icon icon="heart" size="2xl"/>  
+      </div>
+       
+      </div>
+    </header>
+  </div>
+  <div class="main-body">
+      <RouterView />
+  </div>
+
+  <footer>
+   
+  <h1>Getting Started with Vue.js and Font Awesome</h1>
+  </footer>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+header {  
+  background-color: var(--vt-c-blue);
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  gap: 50px;
+  align-items: center;
+  color: var(--vt-c-white);
+}
+
+footer{
+  padding: 10px 6px;
+  bottom: 0px;
+}
+
+.phone-number {
+  margin-left: 25px;
+  margin-right: 10px;
+  display: inline-block;
+  text-decoration: none; /* Убираем подчёркивание */
+    border-bottom: 0 solid var(--vt-c-indigo); /* Параметры линии снизу */
+    transition: 0.2s; /* Время перехода */
+}
+
+.icons {
+  display: flex;
+   justify-content: center;
+   align-items: center;
+  width: 32px;
+  gap: 10px;
+  height: 32px;
+
+}
+
+.icon {
+  transition: 0.2s;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  display: inline-block;
+}
+.main-body {
+    margin: 0 auto;
+    width: 80%;
+  
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.ads{
+  background-color: var(--vt-c-indigo);
+  display: inline-block;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  transform: rotate(-2deg);
+  transition: 0.2s;
 }
+
+.ads:hover {
+  transform: rotate(-4deg);
+}
+
+
+
+nav {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  align-items: center;
+  font-size: 16px;
+  position: relative;
+
+
+}
+
+
 
 nav a.router-link-exact-active {
   color: var(--color-text);
+  position: relative;
+  text-decoration: none; /* Убираем подчёркивание */
+  border-bottom: 0 solid var(--vt-c-indigo); /* Параметры линии снизу */
+  line-height: 4.8;
+  transition: 0.2s; /* Время перехода */
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+nav a.router-link-exact-active:after {
+  display: block;
+  content: "";
+  height: 3px;
+  left: 50%;
+  width: 0;
+  position: absolute;
+  background-color: var(--vt-c-indigo);;
+  transition: all 0.4s;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+
+nav a.router-link-exact-active:hover:after, nav a.router-link-exact-active:focus:after {
+  width: 100%;
+  left: 0;
 }
 
-nav a:first-of-type {
-  border: 0;
+nav a.router-link-exact-active:hover, .phone-number:hover, .icon:hover {
+  color:var(--vt-c-indigo); /* Цвет ссылок при наведении */
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
 import router from './router'
 
+library.add(faPhone)
+library.add(faHeart)
+
 const app = createApp(App)
+
+app.component("font-awesome-icon", FontAwesomeIcon)
 
 app.use(router)
 
